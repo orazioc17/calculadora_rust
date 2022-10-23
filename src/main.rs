858e53b3502cc3cebed279733517f35091068fb5
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-10-23 11:44:16 -0400

    Calculadora funcionando, falta mejorar

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,53 @@
 use regex::Regex;
 
+enum TipoOperacion {
+    SUMA,
+    RESTA,
+    MULTIPLICACION,
+    DIVISION,
+}
+
+fn loop_operaciones(string_regex: Regex, mut expresion: String, tipo_operacion: TipoOperacion) -> String {
+    loop {
+        // Aplicar Operaciones
+        let caps = string_regex.captures(expresion.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+
+        let caps = caps.unwrap();
+
+        let cap_expresion = caps.get(0).unwrap().as_str();
+
+        // Aqui primero se accede a la posicion 1, se pasa a str, luego se parsea a f32 y un ultimo unwrap
+        let left_value: f32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: f32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        println!("{:?} izq: {} der: {}", caps, left_value, right_value); // :? dentro de los corchetes hace que de mas informacion
+        println!("{}", expresion);
+        let resultado: f32;
+
+        match tipo_operacion {
+            TipoOperacion::SUMA => resultado = left_value + right_value,
+            TipoOperacion::RESTA => resultado = left_value - right_value,
+            TipoOperacion::MULTIPLICACION => resultado = left_value * right_value,
+            TipoOperacion::DIVISION => resultado = left_value / right_value,
+        }
+
+        expresion = expresion.replace(cap_expresion, &resultado.to_string()); // amperson al principio transforma un String a un str
+    }
+    return expresion;
+}
+
 fn main() {
-    println!("Hello, world!");
 
     // Regex
-    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
-    let re_mult = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    // Esta es la regex que me funciono para este caso y que desarrolle sobre la marcha, buscar mejor solucion
+    let re_suma = Regex::new(r"(\d+\.?\d*)\s*\+\s*(\d+\.?\d*)").unwrap();
+    let re_mult = Regex::new(r"(\d+\.?\d*)\s*\*\s*(\d+\.?\d*)").unwrap();
+    let re_resta = Regex::new(r"(\d+\.?\d*)\s*\-\s*(\d+\.?\d*)").unwrap();
+    let re_division = Regex::new(r"(\d+\.?\d*)\s*/\s*(\d+\.?\d*)").unwrap();
 
     // (\d+) \s? \+ \s? (\d+)
     /*
@@ -23,54 +65,11 @@ fn main() {
     let mut expresion = String::new();
     println!("Ingrese la expresion: ");
     std::io::stdin().read_line(&mut expresion).unwrap();
-
-    // loop multiplicacion
-    loop {
-        // Aplicar Operaciones
-        let caps = re_mult.captures(expresion.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expresion = caps.get(0).unwrap().as_str();
-
-        // Aqui primero se accede a la posicion 1, se pasa a str, luego se parsea a i32 y un ultimo unwrap
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        println!("{:?} izq: {} der: {}", caps, left_value, right_value); // :? dentro de los corchetes hace que de mas informacion
-
-        let mult = left_value * right_value;
-
-        expresion = expresion.replace(cap_expresion, &mult.to_string()); // amperson al principio transforma un String a un str
-    }
-
-    // loop suma
-    loop {
-        // Aplicar Operaciones
-        let caps = re_add.captures(expresion.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expresion = caps.get(0).unwrap().as_str();
-
-        // Aqui primero se accede a la posicion 1, se pasa a str, luego se parsea a i32 y un ultimo unwrap
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        println!("{:?} izq: {} der: {}", caps, left_value, right_value); // :? dentro de los corchetes hace que de mas informacion
-
-        let suma = left_value + right_value;
-
-        expresion = expresion.replace(cap_expresion, &suma.to_string()); // amperson al principio transforma un String a un str
-    }
+ 
+    expresion = loop_operaciones(re_mult, expresion, TipoOperacion::MULTIPLICACION);
+    expresion = loop_operaciones(re_division, expresion, TipoOperacion::DIVISION);
+    expresion = loop_operaciones(re_suma, expresion, TipoOperacion::SUMA);
+    expresion = loop_operaciones(re_resta, expresion, TipoOperacion::RESTA);
 
     // Mostrar resultado
     println!("Resultado: {}", expresion);
